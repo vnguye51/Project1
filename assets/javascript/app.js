45c3fd4bb136b452840e6251ef7564bdab44ef7b
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-08-16 21:46:07 -0700

    google maps stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,8 @@
 var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
+var location;
+
+var initMap
+
 
 console.log('test')
 var queryURL = "http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=X1-ZWz18f1y9es74b_7x5fi&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA";
@@ -16,3 +14,29 @@ $.ajax({
     var data = response.data;
     console.log(response)
   })
+
+
+  var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=New York,NY&key=AIzaSyAZn90iyzUTnVjifVYvQh7uWUczvW-UsMo'
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+    .then(function(response) {
+      var location = response.results[0].geometry.location;
+      initMap = function(){
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: location,
+          zoom: 8
+        });
+        var marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          title: 'Hello World!'
+        });
+
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+        
+      }
+    })
+  
