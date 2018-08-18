9a0d147a5650d3174480390d4b0ac9f122cadfab
amicokaur <amica0811@yaho..com>
2018-08-18 12:22:10 -0700

    push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,54 +47,50 @@ var jobType;
 var jobCompany;
 var jobPosted;
 
-$.ajax({
-  url: "https://cors-anywhere.herokuapp.com/" + "https://jobs.github.com/positions.json?description=python&location=new+york",
-  method: "GET"
-}).then(function (response) {
-  responseArray = []
-  //grab data from ajax respone and you can see it in console
-  //loop through resonse so dont have to type twice
-  for (var i = 0; i < response.length; i++) {
-    jobTitle = response[i]['title']
-    jobLocation = response[i]["location"]
-    jobType = response[i]["type"]
-    jobCompany = response[i]["company"]
-    jobPosted = response[i]["created_at"]
-
-    responseArray.push(response[i])
-
-//add row
-  var newRow = $('<tr>')
-  newRow.append("<td>" + jobPosted + "</td>","<td>" + jobType + "</td>","<td>" + jobTitle + "</td>", '<td>' + jobLocation + '</td>',"<td>" + jobCompany + "</td>")
-  newRow.addClass('jobEntry')
-  // newRow.attr('data', response[i])
-  $('tbody').append(newRow)
-}})
-
 $("#search").on("click", function (event){ 
-
-
- 
-  console.log('ajsdf') 
+  event.preventDefault()
+console.log('ajsdf') 
   
   var jobSearch = $("#jsearchInput").val().trim() 
   var jobState = $("#stateInput").val().trim() 
+
+  $.ajax({
+    url: "https://cors-anywhere.herokuapp.com/" + "https://jobs.github.com/positions.json?description="+jobSearch+"&location="+jobState,
+    method: "GET"
+  }).then(function (response) {
+    responseArray = []
+    //grab data from ajax respone and you can see it in console
+    //loop through resonse so dont have to type twice
+    for (var i = 0; i < response.length; i++) {
+      jobTitle = response[i]['title']
+      jobLocation = response[i]["location"]
+      jobType = response[i]["type"]
+      jobCompany = response[i]["company"]
+      jobPosted = response[i]["created_at"]
   
-console.log(jobSearch) 
-  }) 
+      responseArray.push(response[i])
+  
+console.log(jobSearch,jobState) 
+  }}) 
 
 
   $(document).on('click','.jobEntry',function(){ 
  sessionStorage.responseArray = JSON.stringify(responseArray); 
    window.open('detailstest.html', '_blank'); 
-  
-  
-   
-   
-  
-  
-   
+
   }) 
+
+  
+  //add row
+    var newRow = $('<tr>')
+    newRow.append("<td>" + jobPosted + "</td>","<td>" + jobType + "</td>","<td>" + jobTitle + "</td>", '<td>' + jobLocation + '</td>',"<td>" + jobCompany + "</td>")
+    newRow.addClass('jobEntry')
+    // newRow.attr('data', response[i])
+    $('tbody').append(newRow)
+  })
+
+
+
 
 
 
