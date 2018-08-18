92183a824b2bcdb87646e87aff968bc987cd6a25
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-08-17 22:31:29 -0700

    map css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,19 +58,19 @@ $.ajax({
     jobLocation = response[i]["location"]
     jobType = response[i]["type"]
     jobCompany = response[i]["company"]
-    jobPosted = response[i]["created_at"]
+    jobPosted = (response[i]["created_at"]).slice(0,10)
     responseArray.push(response[i])
   //add row
   var newRow = $('<tr>')
   newRow.append("<td>" + jobPosted + "</td>","<td>" + jobType + "</td>","<td>" + jobTitle + "</td>", '<td>' + jobLocation + '</td>',"<td>" + jobCompany + "</td>")
   newRow.addClass('jobEntry')
-  // newRow.attr('data', response[i])
+  newRow.attr('data', i)
   $('tbody').append(newRow)
 }})
 
 $(document).on('click','.jobEntry',function(){
-  sessionStorage.responseArray = JSON.stringify(responseArray);
-  window.open('detailstest.html', '_blank');
+  sessionStorage.responseArray = JSON.stringify(responseArray[+$(this).attr('data')]);
+  window.open('detailTab.html', '_blank');
 
 })
 
--- assets/javascript/details.js
+++ assets/javascript/details.js
@@ -1,2 +1,57 @@
 console.log(JSON.parse(sessionStorage.responseArray))
 
+var jobInfo = JSON.parse(sessionStorage.responseArray)
+
+$('#jobInfo').append(jobInfo.company, jobInfo.title,jobInfo.description,jobInfo.location)
+
+var map;
+var geocoder
+var address = jobInfo.location
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay = new google.maps.DirectionsRenderer();
+function initMap() {
+
+    var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+    var mapOptions = {
+      zoom:7,
+      center: chicago
+    }
+    geocoder = new google.maps.Geocoder()
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    directionsDisplay.setMap(map);
+  }
+
+  function geocodeAddress(address, geocoder, resultsMap) {
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+          map: resultsMap,
+          position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+  
+function calcRoute() {
+    var request = {
+        origin: 'UC Berkeley',
+        destination: jobInfo.location,
+        travelMode: 'DRIVING',
+        drivingOptions: {
+            departureTime: new Date(Date.now()),  // for the time N milliseconds from now.
+            trafficModel: 'optimistic'
+        }
+        }
+    directionsService.route(request, function(response, status) {
+        if (status == 'OK') {
+        directionsDisplay.setDirections(response);
+        }
+    });
+}
+
+initMap()
+geocodeAddress(address,geocoder,map)
+calcRoute()
